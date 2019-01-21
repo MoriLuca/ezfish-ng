c5142409cdc880b2b46c91eb1c1855e9cbd46eb4
Luca Mori <Luca.Mori@hotmail.it>
2019-01-21 14:14:11 +0100

    t

---
+++ src/app/componenets/test-csv/test-csv.component.spec.ts
@@ -0,0 +1,25 @@
+import { async, ComponentFixture, TestBed } from '@angular/core/testing';
+
+import { TestCsvComponent } from './test-csv.component';
+
+describe('TestCsvComponent', () => {
+  let component: TestCsvComponent;
+  let fixture: ComponentFixture<TestCsvComponent>;
+
+  beforeEach(async(() => {
+    TestBed.configureTestingModule({
+      declarations: [ TestCsvComponent ]
+    })
+    .compileComponents();
+  }));
+
+  beforeEach(() => {
+    fixture = TestBed.createComponent(TestCsvComponent);
+    component = fixture.componentInstance;
+    fixture.detectChanges();
+  });
+
+  it('should create', () => {
+    expect(component).toBeTruthy();
+  });
+});
---
+++ src/app/componenets/test-csv/test-csv.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-test-csv',
+  templateUrl: './test-csv.component.html',
+  styleUrls: ['./test-csv.component.css']
+})
+export class TestCsvComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
